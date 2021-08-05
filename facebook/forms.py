b83d30705117ecad8b1d173ff49c2f42a6f0f1e6
hyunmin0317 <choihm9903@naver.com>
2021-08-05 12:05:15 +0900

    Update form

--- facebook/forms.py
+++ facebook/forms.py
@@ -6,3 +6,15 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post    # 사용할 모델
         fields = ['title', 'image', 'content']     # PostForm에서 사용할 Post 모델의 속성
+
+        labels = {
+            'title': '제목',
+            'image': '이미지',
+            'content': '내용',
+        }
+
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'image': forms.FileInput(attrs={'class': 'form-control', 'rows': 10}),
+            'content': forms.Textarea(attrs={'class': 'form-control', 'rows': 10}),
+        }
