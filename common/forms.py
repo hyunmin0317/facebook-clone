cdd87e45e227daf537c7acae439b38aaf6a8de58
hyunmin0317 <choihm9903@naver.com>
2021-08-07 14:55:57 +0900

    Update profile

--- common/forms.py
+++ common/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import Profile
 
 
 class UserForm(UserCreationForm):
@@ -9,3 +10,16 @@ class UserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ("username", "password1", "password2", "email")
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']
+
+        labels = {
+            'image': '프로필 사진',
+        }
+
+        widgets = {
+            'image': forms.FileInput(attrs={'class': 'form-control', 'rows': 10}),
+        }
--- common/views.py
+++ common/views.py
@@ -1,21 +1,27 @@
 from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
-from common.forms import UserForm
+from common.forms import UserForm, ProfileForm
 from .models import Profile
 
 # 계정생성
 def signup(request):
     if request.method == "POST":
         form = UserForm(request.POST)
+        forms = ProfileForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)  # 사용자 인증
-            profile = Profile(user=user)
+
+            profile = forms.save(commit=False)
+            profile.user = user
             profile.save()
+
             login(request, user)  # 로그인
             return redirect('home')
     else:
         form = UserForm()
-    return render(request, 'common/signup.html', {'form': form})
+        forms = ProfileForm()
+    context = {'form': form, 'forms': forms}
+    return render(request, 'common/signup.html', context)
