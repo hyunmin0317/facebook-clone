d38e6f6131cbe5469ecead44e0a3895833697a8f
hyunmin0317 <choihm9903@naver.com>
2021-08-07 15:38:28 +0900

    Update profile-image

--- facebook/models.py
+++ facebook/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
+
+from common.models import Profile
 
 class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_post')
@@ -12,6 +15,10 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def profile(self):
+        profile = get_object_or_404(Profile, user=self.author)
+        return profile
 
 class Comment(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
